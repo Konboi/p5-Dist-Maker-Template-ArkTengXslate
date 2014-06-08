808de11bb714061b2067466d4c5ade23be2cd081
Konboi <ryosuke.yabuki@gmail.com>
2014-06-08 23:59:05 +0900

    Added dev.psgi

--- lib/Dist/Maker/Template/ArkTengXslate.pm
+++ lib/Dist/Maker/Template/ArkTengXslate.pm
@@ -58,6 +58,24 @@ our $VERSION = '0.01';
 </body>
 </html>
 
+@@ dev.psgi
+
+use Plack::Builder;
+use Plack::Middleware::Static;
+use lib 'lib';
+use <: $dist.module :>;
+
+my $app = <: $dist.module :>->new;
+$app->setup;
+
+builder {
+    enable 'Plack::Middleware::Static',
+        path => qr{^/(js/|css/|swf/|images?/|imgs?/|static/|[^/]+\.[^/]+$)},
+        root => $app->path_to('root')->stringify;
+    $app->handler;
+};
+
+
 @@ lib/<: $dist.path :>/Models.pm
 package <: $dist.module :>::Models;
 use strict;
